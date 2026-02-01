96dc6ccb8b7b434afe2028a63ea4f5b5bd472775
cipiisekk <papousek.mail@gmail.com>
2026-02-01 14:55:16 +0100

    Added epsilon for better zer0, updated README file

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 Simple quadratic equation in C
 
 it can be done better
-like double isnt 100% correct - 0 can be like 0.000000001 and cuz of that the condition if(x == 0)... is bad
-Most things can be coded better...
+
--- quadraticEquation.c
+++ quadraticEquation.c
@@ -19,11 +19,19 @@ int main(void){
    printf("Napis cislo c: ");
    scanf("%lf", &c);
 
-   d = (c==0) ? (b * b) : (b==0) ? -4 * a * c: (b*b) -4 * a * c;
+   d = my_round(b) - 4 * a * c;
+   
+   double epsilon = 1e-9; // 0.0000000009
 
-   if (d == 0) {
+   if (my_fabs(d) < epsilon) { 
+       /* 
+        * absolutni hodnota cisla (prevedeme ho na kladne)
+        * A pokud je to kladne mensi jako epsilon (0.0000000009xxx)
+        * tak to beru jako nulu
+        */
        printf("Kvadraticka rovnice ma jen jedno reseni: \n");
-       x1 = ((-b) + sqrt(d)) / (2 * a);
+       x1 = (-b) / (2 * a);
+
        intX1 = (int)x1;
        if (intX1 < x1) {
            printf("x = %.10lf\n", x1);
@@ -31,14 +39,15 @@ int main(void){
            printf("x = %.0lf\n", x1);
        }
 
-   } else if (d < 0) {
+   } else if (d < -epsilon ) { // nebo to muzeme napsat sloziteji my_fabs(d) > epsilon && d < 0 
        printf("Kvadraticka rovnice nema reseni\n");
-   } else if (d > 0) {
+   } else {
        printf("Kvadraticka rovnice ma dve reseni: \n");
        x1 = ((-b) + sqrt(d)) / (2 * a);
        x2 = ((-b) - sqrt(d)) / (2 * a);
        intX1 = (int)x1;
        intX2 = (int)x2;
+
        if (intX1 < x1) {
            printf("x1 = %.10lf\n", x1);
        } else {
